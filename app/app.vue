dd36c39da669d44e617648f3da43503523194e6b
Tjay-d3v <tjay.d3v@gmail.com>
2025-09-18 09:16:34 +0800

    x

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-    <h1 class="text-4xl font-bold">🚀 Tailwind is working!</h1>
+    <h1 class="text-4xl font-bold">🚀 Tailwind is workings!</h1>
   </div>
 </template>
 
