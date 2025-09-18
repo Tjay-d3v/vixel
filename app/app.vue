e3f68af4d9322deae5510506fd83e617cd3cbf31
Tjay-d3v <tjay.d3v@gmail.com>
2025-09-18 08:47:40 +0800

    wip

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    xxXXxx
+    <template>
+      <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+        <h1 class="text-4xl font-bold">🚀 Tailwind is working!</h1>
+      </div>
+    </template>
   </div>
 </template>
